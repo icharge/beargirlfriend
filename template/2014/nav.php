bc9643cb496b78a97ca2c9aef1187f63dfdac4ae
iCharge <charge_n@hotmail.com>
2014-09-21 03:11:53 +0700

    แก้ไข nav และ css เล็กน้อย
    + nav  reg login
    + css   margin-bottom  (post-item)

--- template/2014/nav.php
+++ template/2014/nav.php
@@ -1,13 +1,13 @@
   <div class="nav clearfix">
-    <a href="index.html" class="nav-item button clearfix">
+    <a href="index.php" class="nav-item button clearfix">
       <img class="button-img" alt="หน้าแรก" src="images/home-48.png">
     </a>
     <a href="#" class="nav-item nav-item-2">บทความ</a>
     <a class="nav-item nav-item-3" href="#">เรื่องราว</a>
     <a href="cartoons.html" class="nav-item nav-item-4">การ์ตูน</a>
-    <a class="nav-item nav-item-5" href="#">สร้างการ์ตูน</a>
+    <a class="nav-item nav-item-5" href="create.php">สร้างการ์ตูน</a>
     <div class="nav-right clearfix">
-      <a class="nav-item" href="#">ลงทะเบียน</a>
-      <a class="nav-item" href="#">เข้าสู่ระบบ</a>
+      <a class="nav-item" href="register.php">ลงทะเบียน</a>
+      <a class="nav-item" href="login.php">เข้าสู่ระบบ</a>
     </div>
   </div>
